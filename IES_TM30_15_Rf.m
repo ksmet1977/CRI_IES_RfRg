80c9ca6b8d0f27bcf3458dd833a43ffc0b091687
Kevin Smet <kevin.smet@odisee.be>
2018-04-17 20:08:57 +0200

    Update

---
+++ IES_TM30_15_Rf.m
@@ -0,0 +1,17 @@
+function [Rf,Rfi, T, Duv] = IES_TM30_15_Rf(spd)
+% Calculate IES Rf fidelity index following IESTM-30-15.
+%
+% Input:
+%   spd = spectral power distribution of test source
+%
+% Output:
+%   Rf = general fidelity index
+%   Rfi = special fidelity indices (1:99)
+%   T = correlated color temperature (K)
+%   Duv = distance in CIE 1960 UCS above (+) or below (-) Planckian locus
+%
+% Author: Kevin A.G. Smet
+% date: 26/04/2017
+%--------------------------------------------
+
+% Default test spd (Rf = 80.5) (if nargin == 0).
